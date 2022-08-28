2fb634e9f789c2e4c75603316bcbdcd0e4677b49
Asossosaror <111625299+Asossosaror@users.noreply.github.com>
2022-08-28 20:29:52 +0200

    Changed storage system.
    Changed the storage system from purely object based to having arrays as values for question and correct answer. Also changed some fonts.

--- main.js
+++ main.js
@@ -1,41 +1,61 @@
-var countFinishedAddObj = 0;
+var questionsArray = [];
+var answersArray = [];
 var countMainObj = 0;
-var finishedAddObj = {};
+var addObj = {};
 var headline = '';
+var question = '';
+var correctAnswer = '';
 
 function openPage(url) {
     document.location.href = url;
 }
 
 function addQuestion() {
-    countFinishedAddObj += 1;
-    console.log(countFinishedAddObj);
-    let headline = document.getElementById('headlineFieldInput');
+    questionsArray = JSON.parse(localStorage.getItem("questionsArray")) || [];
+    answersArray = JSON.parse(localStorage.getItem("answersArray")) || [];
+    headline = document.getElementById('headlineFieldInput');
     let question = document.getElementById('questionFieldInput');
     let correctAnswer = document.getElementById('correctAnswerFieldInput');
-    let addObj = {'question': question.value, 'correctAnswer': correctAnswer.value};
-    let idNumber = String(countFinishedAddObj);
-    finishedAddObj['exerciseId' + idNumber] = addObj;
-    console.log("FinishedAddObj");
-    console.log(finishedAddObj);
-    console.log(finishedAddObj['exerciseId1']);
+    if (headline.value != '' && question.value != '' && correctAnswer.value != ''){
+        questionsArray.push(question.value);
+        answersArray.push(correctAnswer.value);
+        // Storing arrays
+        let questionsArray_serialized = JSON.stringify(questionsArray);
+        let answersArray_serialized = JSON.stringify(answersArray);
+        localStorage.setItem("questionsArray", questionsArray_serialized);
+        localStorage.setItem("answersArray", answersArray_serialized);
+        console.log(questionsArray);
+        console.log(answersArray);
+    }
 }
 
 function addSet(){
-    console.log("Initializing protocol 'Finish study set'");
-    mainObj = JSON.parse(localStorage.getItem("mainObj")) || {};
-    for (i in mainObj) {
-        if (mainObj.hasOwnProperty(i)) {
-            countMainObj += 1;
+    console.log("Initializing protocol 'Finish study set'. This message will self-destruct in five seconds (disclaimer: it won't).");
+    questionsArray = JSON.parse(localStorage.getItem("questionsArray")) || [];
+    answersArray = JSON.parse(localStorage.getItem("answersArray")) || [];
+    if (questionsArray != [] && answersArray != []){
+        addObj = {'questions': questionsArray, 'correctAnswers': answersArray};
+        console.log(addObj);
+        console.log(addObj['questions'])
+        mainObj = JSON.parse(localStorage.getItem("mainObj")) || {};
+        for (i in mainObj) {
+            if (mainObj.hasOwnProperty(i)) {
+                countMainObj += 1;
+            }
         }
+        let mainIdNumber = String(countMainObj);
+        mainObj['setId' + mainIdNumber] = {'headline': headline.value, addObj};
+        //Saving mainObj in local storage
+        let mainObj_serialized = JSON.stringify(mainObj);
+        localStorage.setItem("mainObj", mainObj_serialized);
+        console.log('mainObj');
+        console.log(localStorage.getItem("mainObj"));
+        // Reset arrays
+        questionsArray_serialized = JSON.stringify([]);
+        answersArray_serialized = JSON.stringify([]);
+        localStorage.setItem("questionsArray", questionsArray_serialized);
+        localStorage.setItem("answersArray", answersArray_serialized);
     }
-    let mainIdNumber = String(countMainObj + 1);
-    mainObj['setId' + mainIdNumber] = {'headline': headline.value, finishedAddObj};
-    //Saving mainObj in local storage
-    let mainObj_serialized = JSON.stringify(mainObj);
-    localStorage.setItem("mainObj", mainObj_serialized);
-    console.log('mainObj');
-    console.log(localStorage.getItem("mainObj"));
 }
 
 function resetStorage() {
@@ -45,4 +65,12 @@ function resetStorage() {
     localStorage.setItem("mainObj", mainObj_serialized);
     console.log(localStorage.getItem("mainObj"));
 
+    questionsArray_serialized = JSON.stringify([]);
+    answersArray_serialized = JSON.stringify([]);
+    localStorage.setItem("questionsArray", questionsArray_serialized);
+    localStorage.setItem("answersArray", answersArray_serialized);
+    questionsArray = JSON.parse(localStorage.getItem("questionsArray")) || [];
+    answersArray = JSON.parse(localStorage.getItem("answersArray")) || [];
+    console.log(questionsArray);
+    console.log(answersArray);
 }
